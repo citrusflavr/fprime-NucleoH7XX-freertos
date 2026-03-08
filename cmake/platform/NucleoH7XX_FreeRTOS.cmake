f8511793e811893547c6401de03a7fe9e78d3d83
citrusxxx <146155598+citrusxxxx@users.noreply.github.com>
2026-03-07 16:21:28 -0800

    removing bad submodules

--- cmake/platform/NucleoH7XX_FreeRTOS.cmake
+++ cmake/platform/NucleoH7XX_FreeRTOS.cmake
@@ -6,9 +6,9 @@
 if(NOT DEFINED ARDUINO_FQBN)
     message(FATAL_ERROR "Must defined arduino FQBN")
 elseif(CMAKE_SYSTEM_PROCESSOR STREQUAL "arm")
-    set(ARDUINO_TYPES_DIR "${FPRIME_PROJECT_ROOT}/lib/fprime-nucleoH7XX-freertos/cmake/platform/arm/Platform") 
+    set(ARDUINO_TYPES_DIR "${FPRIME_PROJECT_ROOT}/lib/fprime-NucleoH7XX-freertos/cmake/platform/arm/Platform") 
 else()
-    set(ARDUINO_TYPES_DIR "${FPRIME_PROJECT_ROOT}/lib/fprime-nucleoH7XX-freertos/cmake/platform/basic/Platform")
+    set(ARDUINO_TYPES_DIR "${FPRIME_PROJECT_ROOT}/lib/fprime-NucleoH7XX-freertos/cmake/platform/basic/Platform")
 endif()
 
 set(CMAKE_EXECUTABLE_SUFFIX "${FPRIME_ARDUINO_EXECUTABLE_SUFFIX}" CACHE INTERNAL "" FORCE)
@@ -19,5 +19,5 @@ set(CMAKE_EXECUTABLE_SUFFIX "${FPRIME_ARDUINO_EXECUTABLE_SUFFIX}" CACHE INTERNAL
 set(FPRIME_PLATFORM "ArduinoFw")
 
 include_directories("${CMAKE_CURRENT_LIST_DIR}")
-include_directories("${FPRIME_PROJECT_ROOT}/lib/fprime-featherm4-freertos/fprime-arduino/Arduino")
+include_directories("${FPRIME_PROJECT_ROOT}/lib/fprime-NucleoH7XX-freertos/fprime-arduino/Arduino")
 add_fprime_subdirectory("${ARDUINO_TYPES_DIR}")
